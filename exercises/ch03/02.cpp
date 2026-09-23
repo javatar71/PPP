41c52d84463186cd7794c24fdb7c06a74b181f3a
javatar71 <phi@sky2night.space>
2026-09-23 12:20:40 +0300

    exercise 2 chapter 3 solution

---
+++ exercises/ch03/02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    int char_val;
+    char input;
+    while(std::cin >> input){
+        char_val = input;
+        std::cout << input << " " << char_val << "\n";
+        }
+}
